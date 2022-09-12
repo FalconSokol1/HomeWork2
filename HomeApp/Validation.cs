3d152a3de4f3fd0bb2e93bcc1362a478d5e4e960
FalconSokol1 <93593831+FalconSokol1@users.noreply.github.com>
2022-09-12 12:32:08 +0300

    Add files via upload

---
+++ HomeApp/Validation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HomeApp
+{
+    internal class Validation
+    {
+        public static string result;
+        public void CheckingConditions(string login,string password)
+        {
+            
+            User user = new User(login, password);
+            
+            if (user.Login == "rus2312" & user.Password == "sd2182343!")
+                result = "Вы успешно авторизовались";
+
+            else if (user.Login != "rus2312")
+                   result = "Такого пользователя не существует";
+            else if (user.Password != "sd2182343!")
+                result = "Неверный пароль";
+
+        }
+    }
+}
